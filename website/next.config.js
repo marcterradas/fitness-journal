7a71ee6eacf0879931ce6bbc832b2ef15bd4e100
Marc Terradas <terradas.marc@gmail.com>
2023-04-28 12:16:26 +0200

    update next config

--- website/next.config.js
+++ website/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  i18n: {
+    locales: ["en", "es", "cat"],
+    defaultLocale: "en",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

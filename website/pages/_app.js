1107c25990baf052c9aec4c12addfcfa69b55b4e
Marc Terradas <terradas.marc@gmail.com>
2023-05-07 09:06:46 +0200

    import language provider

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,5 +1,10 @@
+import LanguageProvider from "@/infrastructure/components/LanguageProvider";
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <LanguageProvider messages={pageProps.messages}>
+      <Component {...pageProps} />
+    </LanguageProvider>
+  );
 }

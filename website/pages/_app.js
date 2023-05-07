f9b476561a0bff55d77ad9b93729589de8322eb6
Marc Terradas <terradas.marc@gmail.com>
2023-05-07 09:03:17 +0200

    remove language provider

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,10 +1,5 @@
-import LanguageProvider from "@/components/infrastructure/providers/Languages";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <LanguageProvider messages={pageProps.messages}>
-      <Component {...pageProps} />
-    </LanguageProvider>
-  );
+  return <Component {...pageProps} />;
 }

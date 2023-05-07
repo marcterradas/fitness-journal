5a82cc274cd2d9b0c0287e25e4e9e01503d77a1f
Marc Terradas <terradas.marc@gmail.com>
2023-05-07 09:13:38 +0200

    document function

--- website/infrastructure/functions/translations.js
+++ website/infrastructure/functions/translations.js
@@ -1,5 +1,10 @@
 import { useTranslations } from "next-intl";
 
+/**
+ * get translations from specific key
+ * @param {string} key
+ * @returns {function}
+ */
 export function getTranslations(key) {
   return useTranslations(key);
 }

a34ace7b46eb28d0a0098c8bca4e934ed13b6392
Marc Terradas <terradas.marc@gmail.com>
2023-05-14 09:59:36 +0200

    get languages

--- website/infrastructure/functions/information.js
+++ website/infrastructure/functions/information.js
@@ -1,8 +1,17 @@
 import KeyWords from "@/infrastructure/constants/keywords.json";
+import Languages from "@/infrastructure/constants/languages.json";
 
 /**
  * @returns {string}
  */
 export function getKeyWords() {
   return KeyWords.keywords;
+}
+
+/**
+ * @param {string} language
+ * @returns {string | undefined}
+ */
+export function getLanguage(language) {
+  return Languages[language];
 }

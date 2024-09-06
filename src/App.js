b91047322d275e9d68247d2d0e5c6fdd509263a5
Marc Terradas <terradas.marc@gmail.com>
2024-09-06 14:17:26 +0200

    create src folder and move App.js inside it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { registerRootComponent } from 'expo'
 
-export default function App() {
+export function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Fitness Journal</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default registerRootComponent(App)

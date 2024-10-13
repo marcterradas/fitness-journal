09028272080f09d0379938351e1495ca41d82bd1
Marc Terradas <terradas.marc@gmail.com>
2024-10-13 20:53:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import { StatusBar } from 'expo-status-bar'
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { registerRootComponent } from 'expo'
 import { t } from 'i18next'
+import useDeviceType from '@/infrastructure/hooks/useDeviceType'
 
 import '@/infrastructure/i18n'
 
 export function App () {
+  const { isDesktop, isMobile } = useDeviceType()
+
   return (
     <View style={styles.container}>
-      <Text>{t('test')}</Text>
-      <StatusBar style="auto" />
+      {isDesktop && (
+        <View style={styles.sidebar}>
+          <Text>Desktop ...</Text>
+        </View>
+      )}
+      {isDesktop && (
+      <View style={styles.content}>
+        <Text>{t('test')}</Text>
+      </View>
+      )}
+      {isMobile && (
+        <View style={styles.buttonBar}>
+          <Text>mobile ...</Text>
+        </View>
+      )}
     </View>
   )
 }
@@ -17,10 +33,26 @@ export function App () {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+    backgroundColor: '#fff'
+  },
+  sidebar: {
+    width: 200,
+    backgroundColor: '#f0f0f0',
+    padding: 10
+  },
+  buttonBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10,
+    backgroundColor: '#f0f0f0'
+  },
+  content: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
   }
 })
 
-export default registerRootComponent(App)
+registerRootComponent(App)

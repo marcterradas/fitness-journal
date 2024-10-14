4a6b7b422c06369a645c5b315e83de6b9bb5d425
Marc Terradas <terradas.marc@gmail.com>
2024-10-14 18:23:02 +0200

    Remove react import.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { registerRootComponent } from 'expo'
 import { t } from '@/infrastructure/hooks/useTranslations'

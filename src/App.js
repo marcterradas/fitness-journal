005396b46108ef9257ef9d1b5ccf7f7175d1ce25
Marc Terradas <terradas.marc@gmail.com>
2024-10-15 17:41:59 +0200

    Clear App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text } from 'react-native'
 
+import { Box } from '@/infrastructure/components/Box'
 import { useDeviceType } from '@/infrastructure/hooks/useDeviceType'
 import { registerRootComponent } from '@/infrastructure/hooks/useRegisterRootComponent'
-import { t } from '@/infrastructure/hooks/useTranslations'
 import '@/infrastructure/i18n'
 
 export function App () {
   const { isDesktop, isMobile } = useDeviceType()
 
-  return (
-    <View style={styles.container}>
-      {isDesktop && (
-        <View style={styles.sidebar}>
-          <Text>Desktop ...</Text>
-        </View>
-      )}
-      {isDesktop && (
-      <View style={styles.content}>
-        <Text>{t('test')}</Text>
-      </View>
-      )}
-      {isMobile && (
-        <View style={styles.buttonBar}>
-          <Text>mobile ...</Text>
-        </View>
-      )}
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#fff'
-  },
-  sidebar: {
-    width: 200,
-    backgroundColor: '#f0f0f0',
-    padding: 10
-  },
-  buttonBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
-    padding: 10,
-    backgroundColor: '#f0f0f0'
-  },
-  content: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+  if (isDesktop) {
+    return (
+      <Box>
+        <Text>Desktop ...</Text>
+      </Box>
+    )
   }
-})
+
+  if (isMobile) {
+    return (
+      <Box>
+        <Text>Mobile ...</Text>
+      </Box>
+    )
+  }
+}
 
 registerRootComponent(App)

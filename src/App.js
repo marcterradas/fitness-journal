35ba1d68040efd26c30ebb4503c89051c59967e2
Marc Terradas <terradas.marc@gmail.com>
2024-10-16 09:15:53 +0200

    Add desktopSideBar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import { DesktopSideBar } from '@/application/DesktopSideBar'
+
 import '@/infrastructure/i18n'
 import { Box } from '@/infrastructure/components/Box'
 import { Paragraph } from '@/infrastructure/components/Paragraph'
 import { useDeviceType } from '@/infrastructure/hooks/useDeviceType'
 import { registerRootComponent } from '@/infrastructure/hooks/useRegisterRootComponent'
+import { useStyles } from '@/infrastructure/hooks/useStyles'
 
 export function App () {
   const { isDesktop, isMobile } = useDeviceType()
+  const styles = useStyles({
+    app: {
+      display: 'flex',
+      flexDirection: 'row',
+      height: '100vh'
+    }
+  })
 
   if (isDesktop) {
     return (
-      <Box>
-        <Paragraph>Desktop ...</Paragraph>
+      <Box style={styles.app}>
+        <DesktopSideBar />
+        <Paragraph>content ...</Paragraph>
       </Box>
     )
   }

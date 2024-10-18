ad453d01b3771fad48b150943fd1d64cdb1091e7
Marc Terradas <terradas.marc@gmail.com>
2024-10-18 21:38:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import { DesktopLayout } from '@/application/DesktopLayout'
-import { MobileLayout } from '@/application/MobileLayout'
-
 import '@/infrastructure/i18n'
-import { SafeArea } from '@/infrastructure/components/SafeArea'
-import { useDeviceType } from '@/infrastructure/hooks/useDeviceType'
+import { NavigationProvider } from '@/infrastructure/components/NavigationProvider'
 import { registerRootComponent } from '@/infrastructure/hooks/useRegisterRootComponent'
 
 export function App () {
-  const { isMobile } = useDeviceType()
-
   return (
-    <SafeArea>
-      {isMobile ? <MobileLayout /> : <DesktopLayout />}
-    </SafeArea>
+    <NavigationProvider />
   )
 }
 

d6d37f3197751cdcc57589bf264da79efaaaa1a4
Marc Terradas <terradas.marc@gmail.com>
2024-10-18 16:50:00 +0200

    Use layouts

--- src/App.js
+++ src/App.js
@@ -1,61 +1,19 @@
-import { DesktopSideBar } from '@/application/DesktopSideBar'
-import { MobileButtonBar } from '@/application/MobileButtonBar'
-import { MobileNavBar } from '@/application/MobileNavBar'
+import { DesktopLayout } from '@/application/DesktopLayout'
+import { MobileLayout } from '@/application/MobileLayout'
 
 import '@/infrastructure/i18n'
-import { Box } from '@/infrastructure/components/Box'
-import { Paragraph } from '@/infrastructure/components/Paragraph'
-import { SafeArea } from '@/infrastructure/components/SafeArea'
+import { SafeArea } from '@/infrastructure/components/SafeArea'
 import { useDeviceType } from '@/infrastructure/hooks/useDeviceType'
-import { useDimensions } from '@/infrastructure/hooks/useDimensions'
 import { registerRootComponent } from '@/infrastructure/hooks/useRegisterRootComponent'
-import { useStyles } from '@/infrastructure/hooks/useStyles'
 
 export function App () {
-  const { isDesktop, isMobile } = useDeviceType()
-  const { screenHeight, screenWidth } = useDimensions()
-  const styles = useStyles({
-    desktop: {
-      display: 'flex',
-      flexDirection: 'row',
-      height: screenHeight
-    },
-    mobile: {
-      display: 'flex',
-      flexDirection: 'column',
-      height: screenHeight,
-      width: screenWidth,
-      justifyContent: 'space-between'
-    },
-    mobileContent: {
-      flex: 1
-    }
-  })
+  const { isMobile } = useDeviceType()
 
-  if (isDesktop) {
-    return (
-      <SafeArea>
-        <Box style={styles.desktop}>
-          <DesktopSideBar />
-          <Paragraph>Content ...</Paragraph>
-        </Box>
-      </SafeArea>
-    )
-  }
-
-  if (isMobile) {
-    return (
-      <SafeArea>
-        <Box style={styles.mobile}>
-          <Box style={styles.mobileContent}>
-            <MobileNavBar />
-            <Paragraph>Content ...</Paragraph>
-          </Box>
-          <MobileButtonBar />
-        </Box>
-      </SafeArea>
-    )
-  }
+  return (
+    <SafeArea>
+      {isMobile ? <MobileLayout /> : <DesktopLayout />}
+    </SafeArea>
+  )
 }
 
 registerRootComponent(App)

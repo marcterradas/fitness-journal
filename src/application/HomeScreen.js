45dc4935c532123ba220146908ab7a1c198707ee
Marc Terradas <terradas.marc@gmail.com>
2024-10-18 21:06:42 +0200

    Create HomeScreen.js

---
+++ src/application/HomeScreen.js
@@ -0,0 +1,12 @@
+import { Box } from '@/infrastructure/components/Box'
+import { Paragraph } from '@/infrastructure/components/Paragraph'
+
+function HomeScreen () {
+  return (
+    <Box>
+      <Paragraph>Home Screen</Paragraph>
+    </Box>
+  )
+}
+
+export { HomeScreen }

5af8daf1a60b33f5b8dd731db25c252cadcd0226
Marc Terradas <terradas.marc@gmail.com>
2024-10-18 21:06:44 +0200

    Create ProfileScreen.js

---
+++ src/application/ProfileScreen.js
@@ -0,0 +1,12 @@
+import { Box } from '@/infrastructure/components/Box'
+import { Paragraph } from '@/infrastructure/components/Paragraph'
+
+function ProfileScreen () {
+  return (
+    <Box>
+      <Paragraph>Profile Screen</Paragraph>
+    </Box>
+  )
+}
+
+export { ProfileScreen }

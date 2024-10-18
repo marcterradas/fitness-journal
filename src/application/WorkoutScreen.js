7ca58a873440e80228afb1b48a37f888f697929a
Marc Terradas <terradas.marc@gmail.com>
2024-10-18 21:06:56 +0200

    Create WorkoutScreen.js

---
+++ src/application/WorkoutScreen.js
@@ -0,0 +1,12 @@
+import { Box } from '@/infrastructure/components/Box'
+import { Paragraph } from '@/infrastructure/components/Paragraph'
+
+function WorkoutScreen () {
+  return (
+    <Box>
+      <Paragraph>Workout Screen</Paragraph>
+    </Box>
+  )
+}
+
+export { WorkoutScreen }

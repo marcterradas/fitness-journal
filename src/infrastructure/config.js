0477b30c0671ec9b12ca64dc5d2f4da04b823701
Marc Terradas <terradas.marc@gmail.com>
2024-10-18 21:06:36 +0200

    Config, add routes.

--- src/infrastructure/config.js
+++ src/infrastructure/config.js
@@ -1,3 +1,10 @@
 export default {
-  desktopBreakpoint: 1440
+  desktopBreakpoint: 1440,
+  initialRouteName: 'HomeScreen',
+  screens: [
+    { name: 'HomeScreen', component: '@/application/HomeScreen' },
+    { name: 'ProfileScreen', component: '@/application/ProfileScreen' },
+    { name: 'SettingsScreen', component: '@/application/SettingsScreen' },
+    { name: 'WorkoutScreen', component: '@/application/WorkoutScreen' }
+  ]
 }

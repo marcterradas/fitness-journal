3cd2633cb4c20b47b801a6d73bc6984ebfa9a5ed
Marc Terradas <terradas.marc@gmail.com>
2024-10-18 21:06:49 +0200

    Create SettingsScreen.js

---
+++ src/application/SettingsScreen.js
@@ -0,0 +1,12 @@
+import { Box } from '@/infrastructure/components/Box'
+import { Paragraph } from '@/infrastructure/components/Paragraph'
+
+function SettingsScreen () {
+  return (
+    <Box>
+      <Paragraph>Settings Screen</Paragraph>
+    </Box>
+  )
+}
+
+export { SettingsScreen }

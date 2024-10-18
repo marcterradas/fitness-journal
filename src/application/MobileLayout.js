fab3f5040602ad4080a8ff85fdebf355cfa8c62c
Marc Terradas <terradas.marc@gmail.com>
2024-10-18 21:38:09 +0200

    Update MobileLayout.js

--- src/application/MobileLayout.js
+++ src/application/MobileLayout.js
@@ -2,11 +2,10 @@ import { MobileButtonBar } from '@/application/MobileButtonBar'
 import { MobileNavBar } from '@/application/MobileNavBar'
 
 import { Box } from '@/infrastructure/components/Box'
-import { Paragraph } from '@/infrastructure/components/Paragraph'
 import { useDimensions } from '@/infrastructure/hooks/useDimensions'
 import { useStyles } from '@/infrastructure/hooks/useStyles'
 
-function MobileLayout () {
+function MobileLayout ({ children }) {
   const { screenHeight, screenWidth } = useDimensions()
   const styles = useStyles({
     mobile: {
@@ -25,7 +24,7 @@ function MobileLayout () {
     <Box style={styles.mobile}>
       <Box style={styles.mobileContent}>
         <MobileNavBar />
-        <Paragraph>Content ...</Paragraph>
+        {children}
       </Box>
       <MobileButtonBar />
     </Box>

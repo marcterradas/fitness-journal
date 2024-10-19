e3bc73b189b0162100b8f3cf6c1a9e4941f07b1c
Marc Terradas <terradas.marc@gmail.com>
2024-10-19 11:41:19 +0200

    Create Loader.js

---
+++ src/application/Loader.js
@@ -0,0 +1,12 @@
+import { Box } from '@/infrastructure/components/Box'
+import { Paragraph } from '@/infrastructure/components/Paragraph'
+
+function Loader () {
+  return (
+    <Box>
+      <Paragraph>Loading...</Paragraph>
+    </Box>
+  )
+}
+
+export { Loader }

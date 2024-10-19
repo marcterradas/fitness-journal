1236a145b876d5bbbc62f82574ebf0ef81def839
Marc Terradas <terradas.marc@gmail.com>
2024-10-19 11:38:12 +0200

    Update config.js

--- src/infrastructure/config.js
+++ src/infrastructure/config.js
@@ -1,10 +1,3 @@
 export default {
-  desktopBreakpoint: 1440,
-  initialRouteName: 'HomeScreen',
-  screens: [
-    { name: 'HomeScreen', component: '@/application/HomeScreen' },
-    { name: 'ProfileScreen', component: '@/application/ProfileScreen' },
-    { name: 'SettingsScreen', component: '@/application/SettingsScreen' },
-    { name: 'WorkoutScreen', component: '@/application/WorkoutScreen' }
-  ]
+  desktopBreakpoint: 1440
 }

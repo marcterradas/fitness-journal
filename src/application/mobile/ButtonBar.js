7b47d7947946f3ee99c4a9430ce4f93aef66d8a3
Marc Terradas <terradas.marc@gmail.com>
2024-10-25 15:42:36 +0200

    Update ButtonBar, add navigation links

--- src/application/mobile/ButtonBar.js
+++ src/application/mobile/ButtonBar.js
@@ -1,18 +1,39 @@
 import { Box } from '@/infrastructure/components/Box'
+import { Link } from '@/infrastructure/components/Link'
 import { Paragraph } from '@/infrastructure/components/Paragraph'
 import { useStyles } from '@/infrastructure/hooks/useStyles'
 
 function ButtonBar ({ navigation }) {
   const style = useStyles({
     ButtonBar: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
       width: '100%',
       height: 50,
       backgroundColor: 'lightgray'
     }
   })
+
+  function goTo (route) {
+    navigation.navigate(route)
+  }
+
   return (
     <Box style={style.ButtonBar}>
-      <Paragraph>ButtonBar ...</Paragraph>
+      <Link onPress={() => goTo('Home')}>
+        <Paragraph>Home</Paragraph>
+      </Link>
+      <Link onPress={() => goTo('Profile')}>
+        <Paragraph>Profile</Paragraph>
+      </Link>
+      <Link onPress={() => goTo('Workout')}>
+        <Paragraph>Workout</Paragraph>
+      </Link>
+      <Link onPress={() => goTo('Settings')}>
+        <Paragraph>Settings</Paragraph>
+      </Link>
     </Box>
   )
 }

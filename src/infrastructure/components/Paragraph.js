3cd9cdf13c622ee3371e7fa11cff7965f250a08a
Marc Terradas <terradas.marc@gmail.com>
2024-10-27 16:36:39 +0100

    add styles to paragraph

--- src/infrastructure/components/Paragraph.js
+++ src/infrastructure/components/Paragraph.js
@@ -1,8 +1,19 @@
 import { Text } from 'react-native'
 
-function Paragraph ({ children, style }) {
+import { useDeviceType } from '@/infrastructure/hooks/useDeviceType'
+import { useStyles } from '@/infrastructure/hooks/useStyles'
+import { fontSizes } from '@/infrastructure/styles'
+
+function Paragraph ({ children }) {
+  const { isMobile } = useDeviceType()
+
+  const style = useStyles({
+    paragraph: {
+      fontSize: isMobile ? fontSizes.sm : fontSizes.md
+    }
+  })
   return (
-    <Text style={style}>
+    <Text style={style.paragraph}>
       {children}
     </Text>
   )

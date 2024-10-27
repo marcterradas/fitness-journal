495bc7701a30a4d7093eac1a01f92a8181f8dfd0
Marc Terradas <terradas.marc@gmail.com>
2024-10-27 17:12:01 +0100

    Updates styles navabar mobile

--- src/application/mobile/NavBar.js
+++ src/application/mobile/NavBar.js
@@ -1,13 +1,15 @@
 import { Box } from '@/infrastructure/components/Box'
 import { Paragraph } from '@/infrastructure/components/Paragraph'
 import { useStyles } from '@/infrastructure/hooks/useStyles'
+import { spacerHalf } from '@/infrastructure/styles'
 
 function NavBar ({ navigation }) {
   const style = useStyles({
     NavBar: {
       width: '100%',
       height: 50,
-      backgroundColor: 'lightgray'
+      backgroundColor: 'lightgray',
+      padding: spacerHalf
     }
   })
   return (

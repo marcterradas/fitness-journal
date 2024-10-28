6d3996b1bc756ae2b04c1ff3c3f2ab0a6ca8b2e7
Marc Terradas <terradas.marc@gmail.com>
2024-10-28 18:51:28 +0100

    Init change language from settings

--- src/application/screens/Settings.js
+++ src/application/screens/Settings.js
@@ -1,10 +1,17 @@
 import { Box } from '@/infrastructure/components/Box'
 import { Paragraph } from '@/infrastructure/components/Paragraph'
+import { Select } from '@/infrastructure/components/Select'
+import config from '@/infrastructure/config'
 
 function Settings ({ navigation }) {
+  function updateLanguage (language) {
+    console.log(language)
+  }
+
   return (
     <Box>
       <Paragraph>Settings Screen</Paragraph>
+      <Select items={config.languages} onSelectChange={updateLanguage} />
     </Box>
   )
 }

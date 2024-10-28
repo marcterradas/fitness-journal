0b97d7de7084d7a051e73e52d0d9cc457a22ce86
Marc Terradas <terradas.marc@gmail.com>
2024-10-28 18:41:51 +0100

    update test for sortedItems

--- test/utils.js
+++ test/utils.js
@@ -19,4 +19,38 @@ describe('sortItems function', () => {
       { label: 'Three', value: '3' }
     ])
   })
+
+  it('should return an empty array if no items are provided', () => {
+    const items = []
+    const selectedValue = '2'
+
+    const sortedItems = sortItems({ items, selectedValue })
+
+    expect(sortedItems).toEqual([])
+  })
+
+  it('should return the original array if no selected value is provided', () => {
+    const items = [
+      { label: 'One', value: '1' },
+      { label: 'Two', value: '2' },
+      { label: 'Three', value: '3' }
+    ]
+    const selectedValue = ''
+
+    const sortedItems = sortItems({ items, selectedValue })
+
+    expect(sortedItems).toEqual(items)
+  })
+
+  it('should return an empty array if neither items nor selected value are provided', () => {
+    const sortedItems = sortItems({})
+
+    expect(sortedItems).toEqual([])
+  })
+
+  it('should return an empty array if no parameters are passed', () => {
+    const sortedItems = sortItems()
+
+    expect(sortedItems).toEqual([])
+  })
 })

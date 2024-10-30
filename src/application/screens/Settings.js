3f7f6651ae7bcf9d9c9157e1720cf5a83f8ecfdc
Marc Terradas <terradas.marc@gmail.com>
2024-10-30 16:24:15 +0100

    remove test translation key

--- src/application/screens/Settings.js
+++ src/application/screens/Settings.js
@@ -2,10 +2,9 @@ import { Box } from '@/infrastructure/components/Box'
 import { Paragraph } from '@/infrastructure/components/Paragraph'
 import { Select } from '@/infrastructure/components/Select'
 import config from '@/infrastructure/config'
-import { changeLanguage, useTranslation } from '@/infrastructure/i18n'
+import { changeLanguage } from '@/infrastructure/i18n'
 
 function Settings ({ navigation }) {
-  const { t } = useTranslation()
   function updateLanguage (language) {
     changeLanguage(language)
   }
@@ -14,7 +13,6 @@ function Settings ({ navigation }) {
     <Box>
       <Paragraph>Settings Screen</Paragraph>
       <Select items={config.languages} onSelectChange={updateLanguage} />
-      <Paragraph>{t('test')}</Paragraph>
     </Box>
   )
 }

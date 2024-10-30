a2be0c2827d24e6968af29ca064bdd06026bd63c
Marc Terradas <terradas.marc@gmail.com>
2024-10-30 16:24:28 +0100

    Add translations to sidebar (desktop)

--- src/application/desktop/SideBar.js
+++ src/application/desktop/SideBar.js
@@ -2,9 +2,12 @@ import { Box } from '@/infrastructure/components/Box'
 import { Link } from '@/infrastructure/components/Link'
 import { Paragraph } from '@/infrastructure/components/Paragraph'
 import { useStyles } from '@/infrastructure/hooks/useStyles'
+import { useTranslation } from '@/infrastructure/i18n'
 import { spacer, colors } from '@/infrastructure/styles'
 
 function SideBar ({ navigation }) {
+  const { t } = useTranslation()
+
   const styles = useStyles({
     SideBar: {
       display: 'flex',
@@ -29,17 +32,17 @@ function SideBar ({ navigation }) {
     <Box style={styles.SideBar}>
       <Box>
         <Link style={styles.link} onPress={() => goTo('Home')}>
-          <Paragraph>Home</Paragraph>
+          <Paragraph>{t('navigation.label.home')}</Paragraph>
         </Link>
         <Link style={styles.link} onPress={() => goTo('Profile')}>
-          <Paragraph>Profile</Paragraph>
+          <Paragraph>{t('navigation.label.profile')}</Paragraph>
         </Link>
         <Link onPress={() => goTo('Workout')}>
-          <Paragraph>Workout</Paragraph>
+          <Paragraph>{t('navigation.label.workouts')}</Paragraph>
         </Link>
       </Box>
       <Link onPress={() => goTo('Settings')}>
-        <Paragraph>Settings</Paragraph>
+        <Paragraph>{t('navigation.label.settings')}</Paragraph>
       </Link>
     </Box>
   )

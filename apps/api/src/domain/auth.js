70b53a45e1e0eb6bc8f8bfa9966b4d67bd08d988
Marc Terradas <terradas.marc@gmail.com>
2024-11-22 14:02:38 +0100

    init login

--- apps/api/src/domain/auth.js
+++ apps/api/src/domain/auth.js
@@ -1,5 +1,13 @@
-function login (request, reply) {
-  reply.send('login')
+async function login (request, reply) {
+  const username = request.body.username
+  const password = request.body.password
+
+  if (!username) reply.code(400).send({ message: 'username is required' })
+  if (!password) reply.code(400).send({ message: 'password is required' })
+
+  // TODO:
+
+  reply.code(200).send({})
 }
 
 export { login }

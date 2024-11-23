abb6d8cadafc681af44a331e3d0e8abfd71020b7
Marc Terradas <terradas.marc@gmail.com>
2024-11-23 16:03:53 +0100

    API: fix bug

--- apps/api/src/domain/auth.js
+++ apps/api/src/domain/auth.js
@@ -1,6 +1,6 @@
 async function login (request, reply) {
-  const username = request.body.username
-  const password = request.body.password
+  const username = request.body?.username
+  const password = request.body?.password
 
   if (!username) reply.code(400).send({ message: 'username is required' })
   if (!password) reply.code(400).send({ message: 'password is required' })
--- apps/api/test/auth.test.js
+++ apps/api/test/auth.test.js
@@ -1,7 +1,22 @@
 import { describe, it, expect } from 'vitest'
 
-describe('auth', () => {
-  it('should work', () => {
-    expect(true).toBe(true)
+import { server } from '../src/infrastructure/server.js'
+import '../src/application/routes.js'
+
+describe('login', () => {
+  it('login without body', async () => {
+    const response = await server.inject({ method: 'POST', url: '/login' })
+    expect(response.statusCode).toBe(400)
+  })
+  it('login without body', async () => {
+    const response = await server.inject({ method: 'POST', url: '/login', payload: {} })
+    expect(response.statusCode).toBe(400)
+  })
+})
+
+describe('login', () => {
+  it('login with body', async () => {
+    const response = await server.inject({ method: 'POST', url: '/login', payload: { username: 'test', password: 'test' } })
+    expect(response.statusCode).toBe(200)
   })
 })

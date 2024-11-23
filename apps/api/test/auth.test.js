f7320c81c38ee0302e6277be422d87e1cde331d0
Marc Terradas <terradas.marc@gmail.com>
2024-11-23 17:13:42 +0100

    Update auth.test.js

--- apps/api/test/auth.test.js
+++ apps/api/test/auth.test.js
@@ -6,6 +6,9 @@ import '../src/application/routes.js'
 describe('login', () => {
   const method = 'POST'
   const url = '/login'
+  // TODO: Create and more username and password test to .env file.
+  const username = 'test'
+  const password = 'test'
 
   it('login without body', async () => {
     const response = await server.inject({ method, url })
@@ -17,8 +20,18 @@ describe('login', () => {
     expect(response.statusCode).toBe(400)
   })
 
-  it('login with body', async () => {
-    const response = await server.inject({ method, url, payload: { username: 'test', password: 'test' } })
+  it('login without username', async () => {
+    const response = await server.inject({ method, url, payload: { password } })
+    expect(response.statusCode).toBe(400)
+  })
+
+  it('login without password', async () => {
+    const response = await server.inject({ method, url, payload: { username } })
+    expect(response.statusCode).toBe(400)
+  })
+
+  it('login correct credentials', async () => {
+    const response = await server.inject({ method, url, payload: { username, password } })
     expect(response.statusCode).toBe(200)
   })
 })

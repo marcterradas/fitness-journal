a0b941d9f5d2ecf18821f71dca9e9d1c8cda02c2
Marc Terradas <terradas.marc@gmail.com>
2024-11-23 15:24:22 +0100

    Update .eslintrc.cjs

--- apps/app/.eslintrc.cjs
+++ apps/app/.eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     'react/jsx-indent': ['error', 2],
     'react/jsx-indent-props': ['error', 2],
+    strict: ['error', 'global'],
     'import/order': [
       'error',
       {

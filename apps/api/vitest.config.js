c52dde4145c1393ed824c1119e85e48574c33301
Marc Terradas <terradas.marc@gmail.com>
2024-12-18 11:37:36 +0100

    API: fix bug, load .env file in tests.

--- apps/api/vitest.config.js
+++ apps/api/vitest.config.js
@@ -1,7 +1,10 @@
+import { loadEnv } from 'vite'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['**/test/*.{js,tsx,ts}']
+    include: ['**/test/*.{js,tsx,ts}'],
+    // Load .env file without any mode.
+    env: loadEnv('', process.cwd(), '')
   }
 })

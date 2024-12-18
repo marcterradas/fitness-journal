369552dcb202f8b6f9b2f63dc0809bf2c44318a9
Marc Terradas <terradas.marc@gmail.com>
2024-12-18 11:27:36 +0100

    API: init db testing

---
+++ apps/api/test/db.test.js
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest'
+
+import config from '../src/infrastructure/config'
+import { client } from '../src/infrastructure/database'
+
+describe('test database connection', () => {
+  it('should connect to the database', async () => {
+    try {
+      await client.connect()
+      const response = await client.db(config.database).command({ ping: 1 })
+      console.log(response)
+      expect(true).toBe(true)
+    } catch (err) {
+      console.error(err)
+    } finally {
+      await client.close()
+    }
+  })
+})

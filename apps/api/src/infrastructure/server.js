a6693341c83be355e194648089673e258516a347
Marc Terradas <terradas.marc@gmail.com>
2024-12-23 16:00:25 +0100

    API: update server, close mongo connection when close database

--- apps/api/src/infrastructure/server.js
+++ apps/api/src/infrastructure/server.js
@@ -1,6 +1,7 @@
 import Fastify from 'fastify'
 
 import config from './config.js'
+import { client } from './database.js'
 
 const server = Fastify({
   logger: config.environment === 'production'
@@ -10,9 +11,21 @@ async function start () {
   try {
     await server.listen({ port: config.port })
   } catch (err) {
+    console.error(err)
     server.log.error(err)
     process.exit(1)
   }
 }
+
+async function shutDown () {
+  console.log('Shutting down server...')
+  await client.close()
+  console.log('Database connection closed.')
+  process.exit(0)
+}
+
+process.on('SIGINT', shutDown)
+process.on('SIGTERM', shutDown)
+process.on('exit', shutDown)
 
 export { server, start }

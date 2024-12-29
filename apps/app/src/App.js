8b93a5d64b0b61d1e6eb628bdf460a57dbd42edc
Marc Terradas <terradas.marc@gmail.com>
2024-12-29 18:59:18 +0100

    APP: if user is not logged in, show loggin.

--- apps/app/src/App.js
+++ apps/app/src/App.js
@@ -1,8 +1,23 @@
 import '@/infrastructure/i18n'
+import { Login as LoginLayout } from '@/application/layouts/Login'
+import Login from '@/application/screens/Login'
+
 import { registerRootComponent } from '@/infrastructure/hooks/useRegisterRootComponent'
 import { Navigation } from '@/infrastructure/providers/Navigation'
+import { useUserStore } from '@/infrastructure/stores/user'
 
 export function App () {
+  const user = useUserStore((state) => state.user)
+
+  // If user is not logged in, show the login screen.
+  if (!user) {
+    return (
+      <LoginLayout>
+        <Login />
+      </LoginLayout>
+    )
+  }
+
   return (
     <Navigation />
   )

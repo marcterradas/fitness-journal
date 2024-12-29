0b5bc229bead9442fa75b8cc6e6b65798ae74cb8
Marc Terradas <terradas.marc@gmail.com>
2024-12-29 16:57:11 +0100

    APP: Init login screen

---
+++ apps/app/src/application/screens/Login.js
@@ -0,0 +1,12 @@
+import { Box } from '@/infrastructure/components/Box'
+import { Paragraph } from '@/infrastructure/components/Paragraph'
+
+function Login ({ navigation }) {
+  return (
+    <Box>
+      <Paragraph>Login Screen</Paragraph>
+    </Box>
+  )
+}
+
+export default Login

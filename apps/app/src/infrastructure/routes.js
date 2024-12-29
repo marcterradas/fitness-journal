a610af7e6a004de124a4b3f06036a2cf490059ee
Marc Terradas <terradas.marc@gmail.com>
2024-12-29 17:18:07 +0100

    APP: update router, add login.

--- apps/app/src/infrastructure/routes.js
+++ apps/app/src/infrastructure/routes.js
@@ -4,12 +4,14 @@ const Home = lazy(() => import('@/application/screens/Home'))
 const Profile = lazy(() => import('@/application/screens/Profile'))
 const Settings = lazy(() => import('@/application/screens/Settings'))
 const Workout = lazy(() => import('@/application/screens/Workout'))
+const Login = lazy(() => import('@/application/screens/Login'))
 
 const routes = [
   { name: 'Home', component: Home },
   { name: 'Profile', component: Profile },
   { name: 'Settings', component: Settings },
-  { name: 'Workout', component: Workout }
+  { name: 'Workout', component: Workout },
+  { name: 'Login', component: Login }
 ]
 
 export { routes }

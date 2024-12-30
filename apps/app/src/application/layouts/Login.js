7119cc1633b80679fb48fa6219a8d28fe5779911
Marc Terradas <terradas.marc@gmail.com>
2024-12-30 11:18:05 +0100

    APP: init login

--- apps/app/src/application/layouts/Login.js
+++ apps/app/src/application/layouts/Login.js
@@ -1,9 +1,21 @@
+import { Box } from '@/infrastructure/components/Box'
 import { SafeArea } from '@/infrastructure/components/SafeArea'
+import { useStyles } from '@/infrastructure/hooks/useStyles'
 
 function Login ({ children }) {
+  const styles = useStyles({
+    Login: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100%'
+    }
+  })
   return (
     <SafeArea>
-      {children}
+      <Box style={styles.Login}>
+        {children}
+      </Box>
     </SafeArea>
   )
 }
--- apps/app/src/application/screens/Login.js
+++ apps/app/src/application/screens/Login.js
@@ -1,10 +1,37 @@
 import { Box } from '@/infrastructure/components/Box'
-import { Paragraph } from '@/infrastructure/components/Paragraph'
+import { Input } from '@/infrastructure/components/Input'
+import { useState } from '@/infrastructure/hooks/useState'
+import { useStyles } from '@/infrastructure/hooks/useStyles'
+import { useTranslation } from '@/infrastructure/i18n'
+import { spacer } from '@/infrastructure/styles'
 
 function Login ({ navigation }) {
+  const { t } = useTranslation()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const styles = useStyles({
+    Login: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 'auto',
+      gap: spacer
+    }
+  })
+
   return (
-    <Box>
-      <Paragraph>Login Screen</Paragraph>
+    <Box style={styles.Login}>
+      <Input
+        placeholder={t('login.label.email')}
+        value={username}
+        onChangeText={setUsername}
+      />
+      <Input
+        placeholder={t('login.label.password')}
+        value={password}
+        onChangeText={setPassword}
+      />
     </Box>
   )
 }

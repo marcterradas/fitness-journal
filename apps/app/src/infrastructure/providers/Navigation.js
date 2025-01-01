a0501784edcd9434f1f1bdca313d97e50a20c59d
Marc Terradas <terradas.marc@gmail.com>
2025-01-01 17:10:17 +0100

    Navigation: change initialRoute to login if user is not logged in.

--- apps/app/src/infrastructure/providers/Navigation.js
+++ apps/app/src/infrastructure/providers/Navigation.js
@@ -2,18 +2,21 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Suspense } from 'react'
 
-import { Default as Layout } from '@/application/layouts/Default'
+import { Layout } from '@/application/Layout'
 import { Loader } from '@/application/Loader'
 
 import config from '@/infrastructure/config'
 import { routes } from '@/infrastructure/routes'
+import { useUserStore } from '@/infrastructure/stores/user'
 
 const Stack = createNativeStackNavigator()
 
 function Navigation () {
+  const user = useUserStore((state) => state.user)
+  const initialRouteName = user ? config.initialRouteName : 'Login'
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={config.initialRouteName}>
+      <Stack.Navigator initialRouteName={initialRouteName}>
         {routes.map((screen, index) => (
           <Stack.Screen
             key={index}

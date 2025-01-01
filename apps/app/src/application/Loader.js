ce81fadfc7ae765bcf99f94c8bba366945fc6175
Marc Terradas <terradas.marc@gmail.com>
2025-01-01 17:28:48 +0100

    APP: Loader add comment to update it.

--- apps/app/src/application/Loader.js
+++ apps/app/src/application/Loader.js
@@ -2,6 +2,7 @@ import { Box } from '@/infrastructure/components/Box'
 import { Paragraph } from '@/infrastructure/components/Paragraph'
 
 function Loader () {
+  // TODO: create loader.
   return (
     <Box>
       <Paragraph>Loading...</Paragraph>

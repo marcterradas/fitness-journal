43236bce4bd97ab6350d988b6f5dfe6fc969f349
Marc Terradas <terradas.marc@gmail.com>
2025-01-02 20:40:51 +0100

    APP: update styles.

--- apps/app/src/infrastructure/components/Select.js
+++ apps/app/src/infrastructure/components/Select.js
@@ -2,6 +2,9 @@ import { Picker } from '@react-native-picker/picker'
 import { useState, useMemo } from 'react'
 
 import { Box } from '@/infrastructure/components/Box'
+import { useDeviceType } from '@/infrastructure/hooks/useDeviceType'
+import { useStyles } from '@/infrastructure/hooks/useStyles'
+import { fontSizes, spacerHalf, borderWidth, borderRadius, colors } from '@/infrastructure/styles'
 
 import { sortItems } from '@/domain/utils'
 
@@ -13,9 +16,23 @@ import { sortItems } from '@/domain/utils'
  * @param {function(string): void} props.onSelectChange - The callback function to call when the selected value changes.
  */
 function Select ({ items = [], onSelectChange = () => {} }) {
+  const { isMobile, isDesktop } = useDeviceType()
   const placeholder = items[0]?.value || ''
   const [selectedValue, setSelectedValue] = useState(placeholder)
   const sortedItems = useMemo(() => sortItems({ items, selectedValue }), [selectedValue, items])
+
+  const styles = useStyles({
+    pickerContainer: {
+      width: '100%',
+      borderWidth,
+      borderRadius,
+      borderColor: colors.black
+    },
+    picker: {
+      fontSize: isMobile ? fontSizes.sm : fontSizes.md,
+      padding: isDesktop ? spacerHalf : 0
+    }
+  })
 
   function handleSelect (itemValue) {
     setSelectedValue(itemValue)
@@ -23,8 +40,9 @@ function Select ({ items = [], onSelectChange = () => {} }) {
   }
 
   return (
-    <Box>
+    <Box style={styles.pickerContainer}>
       <Picker
+        style={styles.picker}
         selectedValue={selectedValue}
         onValueChange={(itemValue) => handleSelect(itemValue)}
       >

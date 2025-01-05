c148ef5e8acb1240ad32afe2290225e65d60181e
Marc Terradas <terradas.marc@gmail.com>
2025-01-05 18:27:30 +0100

    APP: update card, add styles.

--- apps/app/src/infrastructure/components/Card.js
+++ apps/app/src/infrastructure/components/Card.js
@@ -1,8 +1,32 @@
 import { Card as CustomCard } from 'react-native-paper'
 
+import { useStyles } from '@/infrastructure/hooks/useStyles'
+import { borderRadius, spacer } from '@/infrastructure/styles'
+
 function Card ({ title, content, actions, cover, ...props }) {
+  const styles = useStyles({
+    cardContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 'auto',
+      width: '100%',
+      padding: spacer,
+      borderRadius
+    },
+    cardContent: {
+      display: 'flex',
+      height: 'auto',
+      gap: spacer,
+      width: '100%'
+    }
+  })
   return (
-    <CustomCard {...props}>
+    <CustomCard
+      {...props}
+      style={styles.cardContainer}
+      contentStyle={styles.cardContent}
+    >
       {cover && <CustomCard.Cover source={cover} />}
       {title && <CustomCard.Title title={title} />}
       {content && <CustomCard.Content>{content}</CustomCard.Content>}

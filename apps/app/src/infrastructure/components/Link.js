366bea22f6f82205f264a5636884491364d97d42
Marc Terradas <terradas.marc@gmail.com>
2025-01-11 20:02:28 +0100

    APP: format link.

--- apps/app/src/infrastructure/components/Link.js
+++ apps/app/src/infrastructure/components/Link.js
@@ -1,7 +1,13 @@
 import { Pressable } from 'react-native'
 
 function Link ({ children, onPress, style }) {
-  return <Pressable style={style} onPress={onPress}>{children}</Pressable>
+  return (
+    <Pressable
+      style={style}
+      onPress={onPress}>
+      {children}
+    </Pressable>
+  )
 }
 
 export { Link }

98e6a1a5ac62bc76e94a10f6979c73a982ddf41f
Marc Terradas <terradas.marc@gmail.com>
2025-01-11 15:07:22 +0100

    APP: Remove desktop layout.

--- apps/app/src/application/Layout.js
+++ apps/app/src/application/Layout.js
@@ -1,14 +1,10 @@
 import { Layout as LoginLayout } from '@/application/common/LoginLayout'
-import { Layout as DesktopLayout } from '@/application/desktop/Layout'
 import { Layout as MobileLayout } from '@/application/mobile/Layout'
 
 import { SafeArea } from '@/infrastructure/components/SafeArea'
-import { useDeviceType } from '@/infrastructure/hooks/useDeviceType'
 import { useUserStore } from '@/infrastructure/stores/user'
 
 function Layout ({ children, navigation }) {
-  const { isMobile } = useDeviceType()
-  const Layout = isMobile ? MobileLayout : DesktopLayout
   const user = useUserStore((state) => state.user)
 
   if (!user) {
@@ -21,9 +17,9 @@ function Layout ({ children, navigation }) {
 
   return (
     <SafeArea>
-      <Layout navigation={navigation}>
+      <MobileLayout navigation={navigation}>
         {children}
-      </Layout>
+      </MobileLayout>
     </SafeArea>
   )
 }

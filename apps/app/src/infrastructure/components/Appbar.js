ad6ea528e06a12af7093e82cdec10387b132c59c
Marc Terradas <terradas.marc@gmail.com>
2025-01-11 20:23:29 +0100

    APP: Appbar, add title.

--- apps/app/src/infrastructure/components/Appbar.js
+++ apps/app/src/infrastructure/components/Appbar.js
@@ -1,10 +1,17 @@
+import { useRoute } from '@react-navigation/native'
 import { Appbar as PaperAppbar } from 'react-native-paper'
 
+import { useTranslation } from '@/infrastructure/i18n'
+
 function Appbar ({ navigation }) {
+  const route = useRoute()
+  const { t } = useTranslation()
+  const title = t(`navigation.label.${route.name.toLocaleLowerCase()}`)
+
   return (
     <PaperAppbar.Header>
       <PaperAppbar.Content
-        title='title'
+        title={title}
       />
     </PaperAppbar.Header>
   )

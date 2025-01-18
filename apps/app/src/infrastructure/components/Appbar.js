c7681a795e5cfe331a19b2a9806a6c4c1df551b6
Marc Terradas <terradas.marc@gmail.com>
2025-01-18 17:09:13 +0100

    APP: update Appbar, remove title from navigation provider.

--- apps/app/src/infrastructure/components/Appbar.js
+++ apps/app/src/infrastructure/components/Appbar.js
@@ -1,18 +1,9 @@
-import { useRoute } from '@react-navigation/native'
 import { Appbar as PaperAppbar } from 'react-native-paper'
 
-import { useTranslation } from '@/infrastructure/i18n'
-
 function Appbar () {
-  const route = useRoute()
-  const { t } = useTranslation()
-  const title = t(`navigation.label.${route.name.toLocaleLowerCase()}`)
-
   return (
     <PaperAppbar.Header>
-      <PaperAppbar.Content
-        title={title}
-      />
+      <PaperAppbar.Content/>
     </PaperAppbar.Header>
   )
 }

6ed9dbbb6ef1d17ecf9ad4cdc583eeda15f2f006
Marc Terradas <terradas.marc@gmail.com>
2025-01-18 17:42:03 +0100

    APP: update Appbar, show title.

--- apps/app/src/infrastructure/components/Appbar.js
+++ apps/app/src/infrastructure/components/Appbar.js
@@ -1,9 +1,18 @@
 import { Appbar as PaperAppbar } from 'react-native-paper'
 
+import { useTranslation } from '@/infrastructure/i18n'
+import { useNavigationStore } from '@/infrastructure/stores/navigation'
+
 function Appbar () {
+  const { t } = useTranslation()
+  const currentRoute = useNavigationStore((state) => state.currentRoute)
+  const title = t(`navigation.label.${currentRoute}`)
+
   return (
     <PaperAppbar.Header>
-      <PaperAppbar.Content/>
+      <PaperAppbar.Content
+        title={title}
+      />
     </PaperAppbar.Header>
   )
 }

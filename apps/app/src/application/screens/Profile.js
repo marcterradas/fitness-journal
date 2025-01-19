6f7e4976e64812c88fb47ea68c354f2c67f8d42e
Marc Terradas <terradas.marc@gmail.com>
2025-01-19 10:17:37 +0100

    APP: update profile view, remove navigation prop.

--- apps/app/src/application/screens/Profile.js
+++ apps/app/src/application/screens/Profile.js
@@ -1,7 +1,7 @@
 import { Box } from '@/infrastructure/components/Box'
 import { Paragraph } from '@/infrastructure/components/Paragraph'
 
-function Profile ({ navigation }) {
+function Profile () {
   return (
     <Box>
       <Paragraph>Profile Screen</Paragraph>

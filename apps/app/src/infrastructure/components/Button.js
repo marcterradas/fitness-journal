7459140c3afe5171979b961a34548c49b9179cb0
Marc Terradas <terradas.marc@gmail.com>
2025-01-19 12:22:09 +0100

    APP: update button, add color prop.

--- apps/app/src/infrastructure/components/Button.js
+++ apps/app/src/infrastructure/components/Button.js
@@ -3,12 +3,13 @@ import { Button as PaperButton } from 'react-native-paper'
 import { useStyles } from '@/infrastructure/hooks/useStyles'
 import { borderRadius, height } from '@/infrastructure/styles'
 
-function Button ({ children, ...props }) {
+function Button ({ children, color, ...props }) {
   const styles = useStyles({
     button: {
       width: '100%',
       height,
-      borderRadius
+      borderRadius,
+      backgroundColor: color
     }
   })
 

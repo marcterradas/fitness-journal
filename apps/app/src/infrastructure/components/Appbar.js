c88320d2359abee333618b7cacaf98e3112e3ec2
Marc Terradas <terradas.marc@gmail.com>
2025-01-19 13:47:12 +0100

    APP: update appbar, add backgroundColor.

--- apps/app/src/infrastructure/components/Appbar.js
+++ apps/app/src/infrastructure/components/Appbar.js
@@ -1,19 +1,29 @@
 import { Appbar as PaperAppbar } from 'react-native-paper'
 
+import { useStyles } from '@/infrastructure/hooks/useStyles'
 import { useTranslation } from '@/infrastructure/i18n'
 import { useNavigationStore } from '@/infrastructure/stores/navigation'
+import { colors } from '@/infrastructure/styles'
 
 function Appbar () {
   const { t } = useTranslation()
   const currentRoute = useNavigationStore((state) => state.currentRoute)
   const title = t(`navigation.label.${currentRoute}`)
 
+  const styles = useStyles({
+    appbar: {
+      backgroundColor: colors.white
+    }
+  })
+
   return (
-    <PaperAppbar.Header>
-      <PaperAppbar.Content
-        title={title}
-      />
-    </PaperAppbar.Header>
+    <PaperAppbar style={styles.appbar}>
+      <PaperAppbar.Header>
+        <PaperAppbar.Content
+          title={title}
+        />
+      </PaperAppbar.Header>
+    </PaperAppbar>
   )
 }
 

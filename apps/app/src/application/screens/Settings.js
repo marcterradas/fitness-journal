c938ed91d451ffe4a8588b4a8b43fb05b6fd2e2a
Marc Terradas <terradas.marc@gmail.com>
2025-01-19 11:24:16 +0100

    APP: update settings, update styles changeLanguage.

--- apps/app/src/application/screens/Settings.js
+++ apps/app/src/application/screens/Settings.js
@@ -2,17 +2,34 @@ import { Box } from '@/infrastructure/components/Box'
 import { Paragraph } from '@/infrastructure/components/Paragraph'
 import { Select } from '@/infrastructure/components/Select'
 import config from '@/infrastructure/config'
-import { changeLanguage } from '@/infrastructure/i18n'
+import { useStyles } from '@/infrastructure/hooks/useStyles'
+import { changeLanguage, useTranslation } from '@/infrastructure/i18n'
+import { spacer, spacerHalf } from '@/infrastructure/styles'
 
 function Settings () {
+  const { t } = useTranslation()
+  const changeLanguageLabel = `${t('settings.label.changeLanguage')}:`
+
+  const styles = useStyles({
+    content: {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: spacerHalf,
+      padding: spacer
+    }
+  })
+
   function updateLanguage (language) {
     changeLanguage(language)
   }
 
   return (
-    <Box>
-      <Paragraph>Settings Screen</Paragraph>
-      <Select options={config.languages} onValueChange={updateLanguage} />
+    <Box style={styles.content}>
+      <Paragraph>{changeLanguageLabel}</Paragraph>
+      <Select
+        options={config.languages}
+        onValueChange={updateLanguage}
+      />
     </Box>
   )
 }

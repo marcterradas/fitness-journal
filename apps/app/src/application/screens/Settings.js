12f31f5325a9facd6472952e276cd65895892d5f
Marc Terradas <terradas.marc@gmail.com>
2025-01-19 13:03:01 +0100

    APP: update settings, add logout logic.

--- apps/app/src/application/screens/Settings.js
+++ apps/app/src/application/screens/Settings.js
@@ -1,21 +1,33 @@
 import { Box } from '@/infrastructure/components/Box'
+import { Button } from '@/infrastructure/components/Button'
+import { Dialog } from '@/infrastructure/components/Dialog'
 import { Paragraph } from '@/infrastructure/components/Paragraph'
 import { Select } from '@/infrastructure/components/Select'
 import config from '@/infrastructure/config'
+import { useState } from '@/infrastructure/hooks/useState'
 import { useStyles } from '@/infrastructure/hooks/useStyles'
 import { changeLanguage, useTranslation } from '@/infrastructure/i18n'
-import { spacer, spacerHalf } from '@/infrastructure/styles'
+import { useUserStore } from '@/infrastructure/stores/user'
+import { spacer, spacerHalf, colors } from '@/infrastructure/styles'
 
 function Settings () {
   const { t } = useTranslation()
+  const [isDialogVisible, setIsDialogVisible] = useState(false)
+  const logout = useUserStore((state) => state.clearUser)
+
   const changeLanguageLabel = `${t('settings.label.changeLanguage')}:`
 
   const styles = useStyles({
     content: {
       display: 'flex',
       flexDirection: 'column',
-      gap: spacerHalf,
+      gap: spacer,
       padding: spacer
+    },
+    element: {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: spacerHalf
     }
   })
 
@@ -25,10 +37,26 @@ function Settings () {
 
   return (
     <Box style={styles.content}>
-      <Paragraph>{changeLanguageLabel}</Paragraph>
-      <Select
-        options={config.languages}
-        onValueChange={updateLanguage}
+      <Box style={styles.element}>
+        <Paragraph>{changeLanguageLabel}</Paragraph>
+        <Select
+          options={config.languages}
+          onValueChange={updateLanguage}
+        />
+      </Box>
+      <Button
+        icon="logout"
+        color={colors.red}
+        onPress={() => setIsDialogVisible(true)}
+      >
+        {t('settings.label.logout')}
+      </Button>
+      <Dialog
+        visible={isDialogVisible}
+        onDismiss={() => setIsDialogVisible(false)}
+        onConfirm={logout}
+        title={t('settings.label.logout')}
+        content={t('settings.label.confirmLogout')}
       />
     </Box>
   )

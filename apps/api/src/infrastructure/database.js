d56f7ebeb963d07e70f8120fd1aca8b0de56a9de
Marc Terradas <terradas.marc@gmail.com>
2025-02-23 17:37:47 +0100

    API: update databse, export objectid.

--- apps/api/src/infrastructure/database.js
+++ apps/api/src/infrastructure/database.js
@@ -1,8 +1,8 @@
-import mongodb from 'mongodb'
+import mongodb, { ObjectId } from 'mongodb'
 
 import config from './config.js'
 
 const { MongoClient } = mongodb
 const client = new MongoClient(config.mongoDBUri)
 
-export { client }
+export { client, ObjectId }

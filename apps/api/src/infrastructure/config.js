6c6b71690daf27764e301881613727b8c3bedef7
Marc Terradas <terradas.marc@gmail.com>
2025-02-23 17:39:15 +0100

    API: update config, esport testUserId.

--- apps/api/src/infrastructure/config.js
+++ apps/api/src/infrastructure/config.js
@@ -3,6 +3,7 @@ export default {
   database: process.env.DATABASE_NAME,
   environment: process.env.NODE_ENV,
   mongoDBUri: process.env.MONGO_URI,
+  testUserId: process.env.TEST_USER_ID,
   testUserUsername: process.env.TEST_USER_USERNAME,
   testUserPassword: process.env.TEST_USER_PASSWORD,
   usersCollection: 'users'

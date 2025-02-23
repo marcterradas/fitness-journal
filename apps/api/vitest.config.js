fd8c61e6dacefa78d88f97b20decfa3a547da1c9
Marc Terradas <terradas.marc@gmail.com>
2025-02-23 15:32:03 +0100

    API: update vitest config, add folders.

--- apps/api/vitest.config.js
+++ apps/api/vitest.config.js
@@ -3,7 +3,11 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['**/test/*.{js,tsx,ts}'],
+    include: [
+      '**/test/domain/*.{js,tsx,ts}',
+      '**/test/domain/handlers/*.{js,tsx,ts}',
+      '**/test/infrastructure/*.{js,tsx,ts}'
+    ],
     // Load .env file without any mode.
     env: loadEnv('', process.cwd(), '')
   }

37924764f5810374c0e4a398f37ff193c84e72fa
Marc Terradas <terradas.marc@gmail.com>
2025-02-23 17:40:24 +0100

    API: Update, auth test, remove method and url consts.

--- apps/api/test/domain/handlers/auth.test.js
+++ apps/api/test/domain/handlers/auth.test.js
@@ -5,36 +5,33 @@ import { server } from '../../../src/infrastructure/server.js'
 import '../../../src/application/routes.js'
 
 describe('login', () => {
-  const method = 'POST'
-  const url = '/login'
-
   it('login without body', async () => {
-    const response = await server.inject({ method, url })
+    const response = await server.inject({ method: 'POST', url: '/login' })
     expect(response.statusCode).toBe(400)
   })
 
   it('login without body', async () => {
-    const response = await server.inject({ method, url, payload: {} })
+    const response = await server.inject({ method: 'POST', url: '/login', payload: {} })
     expect(response.statusCode).toBe(400)
   })
 
   it('login without username', async () => {
-    const response = await server.inject({ method, url, payload: { password: config.testUserPassword } })
+    const response = await server.inject({ method: 'POST', url: '/login', payload: { password: config.testUserPassword } })
     expect(response.statusCode).toBe(400)
   })
 
   it('login without password', async () => {
-    const response = await server.inject({ method, url, payload: { username: config.testUserUsername } })
+    const response = await server.inject({ method: 'POST', url: '/login', payload: { username: config.testUserUsername } })
     expect(response.statusCode).toBe(400)
   })
 
   it('login wrong credentials', async () => {
-    const response = await server.inject({ method, url, payload: { username: 'test', password: 'test' } })
+    const response = await server.inject({ method: 'POST', url: '/login', payload: { username: 'test', password: 'test' } })
     expect(response.statusCode).toBe(404)
   })
 
   it('login correct credentials', async () => {
-    const response = await server.inject({ method, url, payload: { username: config.testUserUsername, password: config.testUserPassword } })
+    const response = await server.inject({ method: 'POST', url: '/login', payload: { username: config.testUserUsername, password: config.testUserPassword } })
     expect(response.statusCode).toBe(200)
   })
 })

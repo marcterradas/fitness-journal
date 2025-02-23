591ca1f9443d0be8ebccd810b33e2d94103e8318
Marc Terradas <terradas.marc@gmail.com>
2025-02-23 17:40:41 +0100

    API: update router, add get user.

--- apps/api/src/application/routes.js
+++ apps/api/src/application/routes.js
@@ -1,4 +1,7 @@
 import { login } from '../domain/handlers/auth.js'
+import { getUser } from '../domain/handlers/user.js'
 import { server } from '../infrastructure/server.js'
 
 server.post('/login', {}, login)
+
+server.get('/user/:userId', {}, getUser)

2f115e1f3f66b95426165c14f08dddd08653eda7
Marc Terradas <terradas.marc@gmail.com>
2025-03-02 12:16:14 +0100

    API: init exercises.

--- apps/api/src/infrastructure/config.js
+++ apps/api/src/infrastructure/config.js
@@ -8,5 +8,12 @@ export default {
   testUserId: process.env.TEST_USER_ID,
   testUserUsername: process.env.TEST_USER_USERNAME,
   testUserPassword: process.env.TEST_USER_PASSWORD,
-  usersCollection: 'users'
+  usersCollection: 'users',
+  exercises: [
+    { name: 'Bench press', type: 'push' },
+    { name: 'Squat', type: 'legs' },
+    { name: 'Deadlift', type: 'legs' },
+    { name: 'Pull-up', type: 'pull' },
+    { name: 'Dip', type: 'push' }
+  ]
 }

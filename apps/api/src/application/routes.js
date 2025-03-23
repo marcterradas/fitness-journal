5eb1b47dec1495347d7be4f146558111eab1d581
Marc Terradas <terradas.marc@gmail.com>
2025-03-23 21:31:20 +0100

    API: add get workouts route.

--- apps/api/src/application/routes.js
+++ apps/api/src/application/routes.js
@@ -1,7 +1,13 @@
 import { login } from '../domain/handlers/auth.js'
 import { getUser } from '../domain/handlers/user.js'
+import { getWorkouts } from '../domain/handlers/workout.js'
 import { server } from '../infrastructure/server.js'
 
+// Auth.
 server.post('/login', {}, login)
 
+// User.
 server.get('/user/:userId', {}, getUser)
+
+// Workout.
+server.get('/workouts', {}, getWorkouts)

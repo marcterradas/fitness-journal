d9e675b59b3f4a4360e18ad4f6bb724a9d3f13d2
Marc Terradas <terradas.marc@gmail.com>
2025-03-23 21:32:24 +0100

    APP: Update workouts, init get workouts from api.

--- apps/app/src/application/screens/Workout.js
+++ apps/app/src/application/screens/Workout.js
@@ -1,7 +1,24 @@
 import { Box } from '@/infrastructure/components/Box'
 import { Paragraph } from '@/infrastructure/components/Paragraph'
+import { useCallback } from '@/infrastructure/hooks/useCallback'
+import { useEffect } from '@/infrastructure/hooks/useEffect'
+import { getWorkouts } from '@/infrastructure/service/workout'
+import { useNavigationStore } from '@/infrastructure/stores/navigation'
+import { useUserStore } from '@/infrastructure/stores/user'
 
 function Workout () {
+  const currentRoute = useNavigationStore((state) => state.currentRoute)
+  const user = useUserStore((state) => state.user)
+
+  const fetchWorkouts = useCallback(async () => {
+    const workouts = await getWorkouts(user._id)
+    return workouts
+  }, [user._id])
+
+  useEffect(() => {
+    if (currentRoute === 'workout') fetchWorkouts()
+  }, [currentRoute, fetchWorkouts])
+
   return (
     <Box>
       <Paragraph>Workout Screen</Paragraph>
